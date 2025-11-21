c6956a4234b2c5c06281e7b944d209e97e5aa54f
NatalieR2026 <natalie.radin2026@gbwl.org>
2025-11-21 14:40:02 +0000

    11/21

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from matplotlib import pyplot as plt
 # Set style theme
 plt.style.use('seaborn-v0_8-deep')
 
+
 # Read CSV into DataFrame
 df = pd.read_csv('class-data-v1.csv')
-print(df.info())
+df = df.head(8) # Limit to first 8 rows for our class
+
 # NOTE: some cols need type converted (object -> boolean, datetime)
 
+
+
+
+print(df.info())
